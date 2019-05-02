d25aec443134de46487078bb0ba1367a64d72e36
Martin Horn <martin.horn@knime.com>
2019-05-02 15:16:45 +0200

    AP-11682: Utility method added to create 'real' workflow templates in explorer
    ... for testing of local explorer file stores and related stuff.
    
    AP-11682 (Only allow wrapped metanode templates to be added to the node
    repository (local workspace))

--- org.knime.testing/src/org/knime/testing/explorer/LocalFileStoreTestUtils.java
+++ org.knime.testing/src/org/knime/testing/explorer/LocalFileStoreTestUtils.java
@@ -22,9 +22,19 @@ import java.io.File;
 import java.io.OutputStream;
 
 import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.knime.core.node.workflow.SubNodeContainer;
+import org.knime.core.node.workflow.WorkflowContext;
+import org.knime.core.node.workflow.WorkflowCreationHelper;
+import org.knime.core.node.workflow.WorkflowManager;
 import org.knime.core.node.workflow.WorkflowPersistor;
+import org.knime.workbench.explorer.ExplorerActivator;
+import org.knime.workbench.explorer.filesystem.AbstractExplorerFileStore;
 import org.knime.workbench.explorer.filesystem.LocalExplorerFileStore;
 import org.knime.workbench.ui.metainfo.model.MetaInfoFile;
+import org.knime.workbench.ui.preferences.PreferenceConstants;
 
 /**
  * Utility methods for {@link LocalExplorerFileStore}s.
@@ -32,6 +42,8 @@ import org.knime.workbench.ui.metainfo.model.MetaInfoFile;
  * @author Martin Horn, KNIME GmbH, Konstanz, Germany
  */
 public class LocalFileStoreTestUtils {
+
+    private static WorkflowManager TMP_METANODE_ROOT;
 
     private LocalFileStoreTestUtils() {
         // utility class
@@ -82,7 +94,9 @@ public class LocalFileStoreTestUtils {
     }
 
     /**
-     * Creates an empty workflow template/metanode in given filesystem location.
+     * Creates an empty workflow template/metanode in given filesystem location. Empty means that essentially only empty
+     * <tt>workflow.knime</tt> and <tt>template.knime</tt> files are created and nothing else in order represent the
+     * template. This is sufficient for many test cases.
      *
      * @param parent the location in which the template should be created
      * @param name the workflow's name
@@ -110,6 +124,60 @@ public class LocalFileStoreTestUtils {
         }
 
         return newWf;
+    }
+
+    /**
+     * Creates a 'real' template (with no input and output ports) where the respective files
+     * (<tt>workflow.knime, template.knime</tt> etc.) have actual content.
+     *
+     * @param parent the location in which the template should be created
+     * @param name the tempate's name
+     * @param wrap whether to create a 'wrapped' metanode-template to a metanode-template
+     * @return the file store that represents the new template
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends LocalExplorerFileStore> T createTemplate(final T parent, final String name, final boolean wrap) {
+        if (TMP_METANODE_ROOT == null) {
+            TMP_METANODE_ROOT = WorkflowManager.ROOT.createAndAddProject("KNIME Tmp MetaNode Repository",
+                createWorkflowCreationHelper(parent, null));
+        }
+        //deactivate prompt for linking type
+        IPreferenceStore prefStore = ExplorerActivator.getDefault().getPreferenceStore();
+        prefStore.setValue(PreferenceConstants.P_EXPLORER_LINK_ON_NEW_TEMPLATE, MessageDialogWithToggle.NEVER);
+
+        WorkflowManager metaNode =
+            TMP_METANODE_ROOT.createAndAddProject(name, createWorkflowCreationHelper(parent, name));
+        if (wrap) {
+            metaNode.getParent().convertMetaNodeToSubNode(metaNode.getID());
+            boolean success = parent.getContentProvider()
+                .saveSubNodeTemplate((SubNodeContainer)metaNode.getParent().getNodeContainer(metaNode.getID()), parent);
+            assert success;
+        } else {
+            boolean success = parent.getContentProvider().saveMetaNodeTemplate(metaNode, parent);
+            assert success;
+        }
+
+        return (T)parent.getChild(name);
+    }
+
+    private static WorkflowCreationHelper createWorkflowCreationHelper(final AbstractExplorerFileStore parent,
+        final String name) {
+        try {
+            WorkflowCreationHelper workflowCreation = new WorkflowCreationHelper();
+            File currentLocation;
+            if(name != null) {
+                currentLocation =  new File(parent.toLocalFile(), name);
+            } else {
+                currentLocation = parent.toLocalFile();
+            }
+            WorkflowContext.Factory workflowContextFactory =
+                new WorkflowContext.Factory(currentLocation);
+            workflowContextFactory.setMountpointRoot(parent.getContentProvider().getFileStore("/").toLocalFile());
+            workflowCreation.setWorkflowContext(workflowContextFactory.createContext());
+            return workflowCreation;
+        } catch (CoreException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

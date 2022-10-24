97600eef03b2be329b535444cece0f9ed6a1edeb
Carl Witt <carl.witt@knime.com>
2022-10-24 16:30:24 +0200

    Simple node to test properties of the current workflow context. Compares the string representation (toString) of the WorkflowContextV2 to a pattern. The pattern can be either a static string or contain regular expressions enclosed in _!_

---
+++ org.knime.testing.application/src/org/knime/testing/node/workflowcontext/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Node for verifying properties of the workflow context (workflow source location, executor info, etc.)
+ *
+ * @author Carl Witt, KNIME AG, Zurich, Switzerland
+ */
+package org.knime.testing.node.workflowcontext;

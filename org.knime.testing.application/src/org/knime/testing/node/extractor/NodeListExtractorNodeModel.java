93c974e021169272a70f532814a5846c753795a6
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2023-03-08 16:59:49 +0100

    AP-20266: node list extractor with xml type as description
    AP-20266 (Node List Extractor to have xml output)

--- org.knime.testing.application/src/org/knime/testing/node/extractor/NodeListExtractorNodeModel.java
+++ org.knime.testing.application/src/org/knime/testing/node/extractor/NodeListExtractorNodeModel.java
@@ -48,11 +48,14 @@
  */
 package org.knime.testing.node.extractor;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -60,16 +63,21 @@ import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DataTableSpecCreator;
+import org.knime.core.data.DataValue;
 import org.knime.core.data.RowKey;
 import org.knime.core.data.collection.ListCell;
 import org.knime.core.data.def.BooleanCell;
 import org.knime.core.data.def.StringCell;
+import org.knime.core.data.v2.WriteValue;
 import org.knime.core.data.v2.value.ValueInterfaces.BooleanWriteValue;
 import org.knime.core.data.v2.value.ValueInterfaces.StringListWriteValue;
 import org.knime.core.data.v2.value.ValueInterfaces.StringWriteValue;
+import org.knime.core.data.xml.XMLCell;
+import org.knime.core.data.xml.XMLCellFactory;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
@@ -82,6 +90,7 @@ import org.knime.core.node.extension.NodeSetFactoryExtension;
 import org.knime.core.webui.node.dialog.impl.WebUINodeConfiguration;
 import org.knime.core.webui.node.dialog.impl.WebUINodeModel;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 import com.google.common.collect.Streams;
 
@@ -104,6 +113,7 @@ public class NodeListExtractorNodeModel extends WebUINodeModel<NodeListExtractor
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec,
         final NodeListExtractorNodeSettings s) throws Exception {
+        var messageBuilder = createMessageBuilder();
         var spec = createSpec(s);
         try (var tableContainer = exec.createRowContainer(spec);
                 var writeCursor = tableContainer.createCursor()) {
@@ -141,7 +151,21 @@ public class NodeListExtractorNodeModel extends WebUINodeModel<NodeListExtractor
                     ((StringWriteValue)rowWrite.getWriteValue(col++)).setStringValue(singleNode.getFactoryClassname());
                 }
                 if (s.m_includeNodeDescription) {
-                    ((StringWriteValue)rowWrite.getWriteValue(col++)).setStringValue(toDocument(f.getXMLDescription()));
+                    DataCell cell;
+                    try {
+                        cell = toDocument(f.getXMLDescription());
+                    } catch (Exception e) {
+                        final var msg = String.format("Unable to extract node description for %s: %s",
+                            singleNode.getFactoryClassname(), e.getMessage());
+                        messageBuilder.addTextIssue(msg);
+                        getLogger().debug(msg, e);
+                        cell = null;
+                    }
+                    if (cell != null) {
+                        ((WriteValue<DataValue>)rowWrite.getWriteValue(col++)).setValue(cell);
+                    } else {
+                        rowWrite.setMissing(col++);
+                    }
                 }
                 if (s.m_includeKeywords) {
                     final var keywords = f.getKeywords();
@@ -153,6 +177,14 @@ public class NodeListExtractorNodeModel extends WebUINodeModel<NodeListExtractor
                 }
                 rowWrite.setRowKey(RowKey.createRowKey((long)i++));
             }
+            if (messageBuilder.getIssueCount() == 1) {
+                messageBuilder.withSummary(messageBuilder.getFirstIssue().orElseThrow());
+            } else if (messageBuilder.getIssueCount() > 1) {
+                messageBuilder.withSummary(
+                    String.format("There are %d nodes for which the node descriptions can not be extracted.",
+                        messageBuilder.getIssueCount()));
+            }
+            messageBuilder.build().ifPresent(this::setWarning);
             return new BufferedDataTable[] {tableContainer.finish()};
         }
     }
@@ -168,7 +200,7 @@ public class NodeListExtractorNodeModel extends WebUINodeModel<NodeListExtractor
             creator.addColumns(new DataColumnSpecCreator("Node Factory", StringCell.TYPE).createSpec());
         }
         if (settings.m_includeNodeDescription) {
-            creator.addColumns(new DataColumnSpecCreator("Node Description", StringCell.TYPE).createSpec());
+            creator.addColumns(new DataColumnSpecCreator("Node Description", XMLCell.TYPE).createSpec());
         }
         if (settings.m_includeKeywords) {
             creator.addColumns(new DataColumnSpecCreator("Keywords", ListCell.getCollectionType(StringCell.TYPE)).createSpec());
@@ -176,14 +208,17 @@ public class NodeListExtractorNodeModel extends WebUINodeModel<NodeListExtractor
         return creator.createSpec();
     }
 
-    private static String toDocument(final Element xml)
-        throws TransformerFactoryConfigurationError, TransformerException {
+    private static DataCell toDocument(final Element xml) throws TransformerFactoryConfigurationError,
+        TransformerException, IOException, ParserConfigurationException, SAXException, XMLStreamException {
+        // this doesn't work:
+        //      return XMLCellFactory.create(xml.getOwnerDocument());
+        // because ... "DOM Level 3 Not implemented" (xmlbeans problem, apparently)
         DOMSource domSource = new DOMSource(xml);
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         StringWriter sw = new StringWriter();
         StreamResult sr = new StreamResult(sw);
         transformer.transform(domSource, sr);
-        return sw.toString();
+        return XMLCellFactory.create(sw.toString());
     }
 
     interface SingleNode {
